addf428fb702bc57f6bb20493a83f3716ffa56af
PILWON SEO <isungse@naver.com>
2022-04-12 15:43:53 +0900

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 let network = document.getElementById("net-work");
+let barcode = document.getElementById("barcode");
 
-network.addEventListener("click", render);
+network.addEventListener("click", getNetwork);
+barcode.addEventListener("click", getBarcode)
 
-function render() {
-  let resultHTML = "";
-  resultHTML += `<div>
-    <embed src="files/network01.pdf" type="application/pdf" width="100%" height="800px" />
-  </div>`;
+function getNetwork(){
+    let resultHTML = "";
+  
+    resultHTML += `<div>
+      <embed src="files/network01.pdf" type="application/pdf" width="100%" height="800px" />
+    </div>`;
+  
+    document.getElementById("show-pdf").innerHTML = resultHTML;
+}
 
-  document.getElementById("show-pdf").innerHTML = resultHTML;
+function getBarcode(){
+    let resultHTML = "";
+  
+    resultHTML += `<div>
+      <embed src="files/001.pdf" type="application/pdf" width="100%" height="800px" />
+    </div>`;
+  
+    document.getElementById("show-pdf").innerHTML = resultHTML;
 }
