4a82113974cb58e3b0d5c91cf744f9201f6e13db
PILWON SEO <isungse@naver.com>
2022-04-25 07:38:41 +0900

    first commit

--- main.js
+++ main.js
@@ -1,45 +1,16 @@
 let network = document.getElementById("network");
 let barcode = document.getElementById("barcode");
 let ocs = document.getElementById("ocs");
+let phonebook = document.getElementById("phonebook")
 
-network.addEventListener("click", getNetwork);
-barcode.addEventListener("click", getBarcode);
-ocs.addEventListener("click", getOcs);
+//network.addEventListener("click", getNetwork);
+//barcode.addEventListener("click", getBarcode);
+//ocs.addEventListener("click", getOcs);
+
+//phonebook.addEventListener("click", getPhonebook);
 
 function getNetwork() {
   let resultHTML = "";
-
-  resultHTML += `<div class="row">
-      
-    <div class="col-3 box"><div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">공유폴더 접속불가</h5>
-      <a href="network.html" class="card-link">Click</a>
-      
-    </div>
-  </div></div>
-    <div class="col-3 box"><div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">LAN Cable 접속불량</h5>
-      <a href="network.html" class="card-link">Click</a>
-      
-    </div>
-  </div></div>
-      <div class="col-3 box"><div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">TEST1</h5>
-        <a href="#" class="card-link">Card link</a>
-        
-      </div>
-    </div></div>
-      <div class="col-3 box"><div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">TEST2</h5>
-        <a href="#" class="card-link">Card link</a>
-        <p class="card-text"></p> 
-      </div>
-    </div></div>
-  </div>`;
 
   document.getElementById("show-pdf").innerHTML = resultHTML;
 }
@@ -47,14 +18,18 @@ function getNetwork() {
 function getBarcode() {
   let resultHTML = "";
 
-  resultHTML += `<div>
-      <embed src="files/network01.pdf" type="application/pdf" width="100%" height="800px" />
-    </div>`;
-
   document.getElementById("show-pdf").innerHTML = resultHTML;
 }
 
 function getOcs() {
+  let resultHTML = "";
+
+  resultHTML += ``;
+
+  document.getElementById("show-pdf").innerHTML = resultHTML;
+}
+
+function getPhonebook() {
   let resultHTML = "";
 
   resultHTML += ``;
